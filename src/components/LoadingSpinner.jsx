4656e62d7ae55c5ff1641335509a5a3b089742c9
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2024-01-16 19:39:13 +0530

    loading spinner added while loading posts

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+
+
+const LoadingSpinner = () => {
+    return (
+        <div className="d-flex justify-content-center mt-5">
+            <div className="spinner-border" role="status" style={{width: "4rem", height: "4rem"}}>
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingSpinner
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,29 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import Post from "./Post"
 import { PostList as PostListData } from '../store/post-list-store'
 import WelcomeMessage from "./WelcomeMessage"
+import LoadingSpinner from "./LoadingSpinner"
 
 
 const PostList = () => {
     const { postList, fetchDummyPosts } = useContext(PostListData)
+    const [fetching, setFetching ] = useState(false)
 
     useEffect(() => {
+        setFetching(true)
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then(data => {
                 fetchDummyPosts(data.posts)
+                setFetching(false)
             });
-    }, [fetchDummyPosts])
+    }, [])
 
     return (
         <div className="">
-            {postList.length === 0 && <WelcomeMessage />}
-            {postList.map((post) => {
+            {fetching && <LoadingSpinner /> }
+            {!fetching && postList.length === 0 && <WelcomeMessage />}
+            {!fetching && postList.map((post) => {
                 return <Post key={post.id} post={post} />
             })}
         </div>
